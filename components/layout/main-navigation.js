1b81253aeb2dfcbb85d3dcc682739e21655466e1
AndyPew <christianandyp09@gmail.com>
2022-07-13 15:21:45 +0700

    auth

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,41 @@
+import { useSession, signOut } from "next-auth/react"
+import Link from 'next/link';
+
+import classes from './main-navigation.module.css';
+
+export default function MainNavigation() {
+  const {data: session, loading} = useSession()
+
+  function logoutHandler() {
+    signOut();
+  }
+
+  return (
+    <header className={classes.header}>
+      <Link href='/'>
+        <a>
+          <div className={classes.logo}>Next Auth</div>
+        </a>
+      </Link>
+      <nav>
+        <ul>
+        {!session && !loading && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
